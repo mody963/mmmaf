61185f0e16b247879da8320766b169a46e6d0f86
MilanVHR <1034676@hr.nl>
2026-02-23 12:45:43 +0100

    Working English and Dutch WriteLines

---
+++ condensation/Project/Texts.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Resources;
+
+namespace Project;
+// This class is responsible for fetching the correct text based on the current culture.
+public static class Texts
+{
+    private static readonly ResourceManager _resourceManager =
+        new ResourceManager("Project.Resources.Resources",
+            Assembly.GetExecutingAssembly());
+
+    public static string Get(string key)
+    {
+        return _resourceManager.GetString(key)
+               ?? $"Missing: {key}";
+    }
+}
