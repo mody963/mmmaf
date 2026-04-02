e1cdbb50cb023a09b1a701b6e9179dde17c6db99
MilanVHR <1034676@hr.nl>
2026-04-02 13:33:42 +0200

    Sounds testing on mac

--- condensation/Project/Presentation/SoundEffects.cs
+++ condensation/Project/Presentation/SoundEffects.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
-using System.Media;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 public static class SoundEffects
 {
@@ -32,12 +33,72 @@ public static class SoundEffects
     {
         try
         {
-            using var player = new SoundPlayer(path);
-            player.PlaySync();
+            if (!File.Exists(path))
+            {
+                return;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                // Use PowerShell's SoundPlayer on Windows.
+                var windowsCommand =
+                    "Add-Type -AssemblyName System.Media; " +
+                    "$player = New-Object System.Media.SoundPlayer('" + path.Replace("'", "''") + "'); " +
+                    "$player.PlaySync()";
+
+                RunProcess("powershell", "-NoProfile", "-Command", windowsCommand);
+                return;
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                RunProcess("afplay", path);
+                return;
+            }
+
+            // Linux fallback: try common audio players in order.
+            if (TryRunProcess("paplay", path))
+            {
+                return;
+            }
+
+            TryRunProcess("aplay", path);
         }
         catch
         {
             // ignore errors playing sound so it doesn't crash the app
+        }
+    }
+
+    private static void RunProcess(string fileName, params string[] arguments)
+    {
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo
+        {
+            FileName = fileName,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        foreach (var argument in arguments)
+        {
+            process.StartInfo.ArgumentList.Add(argument);
+        }
+
+        process.Start();
+        process.WaitForExit();
+    }
+
+    private static bool TryRunProcess(string fileName, params string[] arguments)
+    {
+        try
+        {
+            RunProcess(fileName, arguments);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
 }
